6b058a08637abba2f127af010a40a42cc85d8e88
eshamard <eshamard@redhat.com>
2023-08-21 12:30:11 +0200

    Title widget

---
+++ testing/components/test_title.py
@@ -0,0 +1,25 @@
+from widgetastic.widget import View
+
+from widgetastic_patternfly5.components.title import Title
+
+
+TESTING_PAGE_URL = "https://patternfly-react-main.surge.sh/components/title"
+
+
+class TitleTestView(View):
+    h1 = Title("h1 defaults to 2xl")
+    h2 = Title("h2 defaults to xl")
+    h3 = Title("h3 defaults to lg")
+    h4 = Title("h4 defaults to md")
+    h5 = Title("h5 defaults to md")
+    h6 = Title("h6 defaults to md")
+
+
+def test_title_text_and_size(browser):
+    view = TitleTestView(browser)
+    for name in view.widget_names:
+        widget = getattr(view, name)
+        assert widget.is_displayed
+        assert widget.text == widget.expected
+        assert widget.read() == widget.expected
+        assert str(widget.heading_level) == str(name)
