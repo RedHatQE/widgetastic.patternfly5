f15be8cf89d3d1197880afa0b9d057fd7fe6a86b
Jakub Rusz <jrusz@redhat.com>
2024-02-19 14:26:40 +0100

    Add basic support for TypeaheadSelect

--- testing/components/menus/test_select.py
+++ testing/components/menus/test_select.py
@@ -4,6 +4,7 @@ from widgetastic.widget import View
 from widgetastic_patternfly5 import CheckboxSelect
 from widgetastic_patternfly5 import Select
 from widgetastic_patternfly5 import SelectItemNotFound
+from widgetastic_patternfly5.components.menus.select import TypeaheadSelect
 
 TESTING_PAGE_URL = "https://patternfly-react-main.surge.sh/components/menus/select"
 
@@ -83,3 +84,51 @@ def test_checkbox_select_item_checkbox_select(checkbox_select):
     with pytest.raises(SelectItemNotFound):
         checkbox_select.fill({"Non existing item": True})
     assert not checkbox_select.is_open
+
+
+@pytest.fixture
+def typeahead_select(browser):
+    class TestView(View):
+        typeahead_select = TypeaheadSelect(locator=".//div[@id='ws-react-c-select-typeahead']")
+
+    return TestView(browser).typeahead_select
+
+
+def test_typeahead_select_is_displayed(typeahead_select):
+    assert typeahead_select.is_displayed
+
+
+def test_typeahead_select_items(typeahead_select):
+    assert set(typeahead_select.items) == {
+        "Alabama",
+        "Florida",
+        "New Jersey",
+        "New Mexico",
+        "New York",
+        "North Carolina",
+    }
+    assert typeahead_select.has_item("Alabama")
+    assert not typeahead_select.has_item("Non existing item")
+    assert typeahead_select.item_enabled("Alabama")
+
+
+def test_typeahead_select_open(typeahead_select):
+    assert not typeahead_select.is_open
+    typeahead_select.open()
+    assert typeahead_select.is_open
+    typeahead_select.close()
+    assert not typeahead_select.is_open
+
+
+def test_typeahead_select_item_select(typeahead_select):
+    typeahead_select.fill("New Mexico")
+    assert typeahead_select.read() == "New Mexico"
+    assert not typeahead_select.is_open
+    # try again to verify it works when an item was selected previously (or in
+    # case there is a default value)
+    typeahead_select.fill("Florida")
+    assert typeahead_select.read() == "Florida"
+    assert not typeahead_select.is_open
+    with pytest.raises(SelectItemNotFound):
+        typeahead_select.fill("Non existing item")
+    assert not typeahead_select.is_open
